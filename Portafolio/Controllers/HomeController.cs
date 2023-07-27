aa6d7bfd070ab8954959b6c6587e1170c204614e
Kevin Lemus <lemus5f@outlook.com>
2023-07-27 17:38:15 -0600

    Agregar archivos de proyecto.

---
+++ Portafolio/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portafolio.Data;
+using Portafolio.Models;
+using System.Diagnostics;
+
+namespace Portafolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private RepositorioProyectos _repositorio = new RepositorioProyectos();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var model = new HomeIndex()
+            {
+                Proyectos = _repositorio.ObtenerProyectos().Take(3)
+            };
+
+            return View(model);
+        }
+        
+        public IActionResult Proyectos()
+        {
+            var proyectos = _repositorio.ObtenerProyectos();
+
+            return View(proyectos);
+        }
+
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contacto(Contacto contacto) 
+        {
+            return RedirectToAction("Gracias");
+        }
+
+        public IActionResult Gracias()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Portafolio/Data/RepositorioProyectos.cs
@@ -0,0 +1,33 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Data
+{
+    public class RepositorioProyectos
+    {
+        public List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto> { new Proyecto
+            {
+                Titulo = "Administrador de escuelas",
+                Descripcion = "Control de la información de varias escuelas realizado en ASP.Net Core",
+                ImagenURL = "/images/webescuela.jpg",
+                Link = ""
+            },
+            new Proyecto
+            {
+                Titulo = "Administrador de escuelas",
+                Descripcion = "Control de la información de varias escuelas realizado en ASP.Net Core",
+                ImagenURL = "/images/webescuela.jpg",
+                Link = ""
+            },
+            new Proyecto
+            {
+                Titulo = "Administrador de escuelas",
+                Descripcion = "Control de la información de varias escuelas realizado en ASP.Net Core",
+                ImagenURL = "/images/webescuela.jpg",
+                Link = ""
+            }
+            };
+        }
+    }
+}
